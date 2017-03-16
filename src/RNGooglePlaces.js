bc16eeed6127eb675027b5064f15f97091e98e7c
Sathish <sathish.s@atomicits.com>
2017-03-16 11:03:52 +0530

    Types are changed

--- src/RNGooglePlaces.js
+++ src/RNGooglePlaces.js
@@ -6,7 +6,7 @@ exports._openAutocompleteModal = function(filterOptions){
     return function(success_callback){
         return function(error_callback){
             return function(){
-                rnxGooglePlaces._openAutocompleteModal(filterOptions).then(function(resData){
+                rnxGooglePlaces.openAutocompleteModal(filterOptions).then(function(resData){
                     success_callback(resData)();
                 }).catch(function(error){
                     error_callback(error)();
@@ -20,7 +20,7 @@ exports._openPlacePickerModal = function(boundsOptions){
     return function(success_callback){
         return function(error_callback){
             return function(){
-                rnxGooglePlaces._openPlacePickerModal(boundsOptions).then(function (resData){
+                rnxGooglePlaces.openPlacePickerModal(boundsOptions).then(function (resData){
                     success_callback (resData)();
                 }).catch(function (error){
                     error_callback(error)();
@@ -35,7 +35,7 @@ exports._getAutocompletePredictions = function(queryOptions){
     return function(success_callback){
         return function(error_callback){
             return function(){
-                rnxGooglePlaces._getAutocompletePredictions(queryOptions).then(function (resData){
+                rnxGooglePlaces.getAutocompletePredictions(queryOptions).then(function (resData){
                     success_callback (resData)();
                 }).catch(function (error){
                     error_callback(error)();
@@ -51,7 +51,7 @@ exports._lookUpPlaceByID = function(placeID){
     return function(success_callback){
         return function(error_callback){
             return function(){
-                rnxGooglePlaces._lookUpPlaceByID(placeID).then(function (resData){
+                rnxGooglePlaces.lookUpPlaceByID(placeID).then(function (resData){
                     success_callback(resData)();
                 }).catch(function(error){
                     error_callback(error)();
--- src/RNGooglePlaces.purs
+++ src/RNGooglePlaces.purs
@@ -6,22 +6,54 @@ import Control.Monad.Aff (Aff, attempt, makeAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (Error)
 import Data.Either (Either)
-import Unsafe.Coerce (unsafeCoerce)
 
-foreign import data FilterOptions :: *
-foreign import data BoundOptions :: *
-foreign import data Query :: *
 
-filterType :: forall a. a -> FilterOptions
-filterType = unsafeCoerce
+type FilterOptions =
+  { type      :: String
+  , country    :: String
+  , latitude   :: Number
+  , longitude  :: Number
+  , radius     :: Number
+  , useOverlay :: Number
+  }
 
-boundType :: forall a. a -> BoundOptions
-boundType = unsafeCoerce
+type BoundOptions =
+  { latitude  :: Number
+  , longitude :: Number
+  , radius    :: Number
+  }
 
-queryType :: forall a. a -> Query
-queryType = unsafeCoerce
+type Query =
+  { type      :: String
+  , country    :: String
+  , latitude   :: Number
+  , longitude  :: Number
+  , radius     :: Number
+  }
 
 newtype PlaceId = PlaceId String
+
+type ResponseModal  =
+  { placeID     :: String
+  , website     :: String
+  , phoneNumber :: String
+  , address     :: String
+  , name        :: String
+  , types       :: Array String
+  , latitude    :: Number
+  , longitude   :: Number
+  }
+
+type AutocompletePredictionsResponseModal = Array APResponseModal
+
+type APResponseModal =
+  { primaryText :: String
+  , placeID :: String
+  , secondaryText :: String
+  , fullText :: String
+  , types :: Array String
+  }
+
 
 foreign import _openAutocompleteModal ::
   forall e scallback ecallback. FilterOptions
@@ -47,14 +79,15 @@ foreign import _lookUpPlaceByID ::
   -> ecallback
   -> Eff e Unit
 
-openautoCompleteModal :: forall a b. FilterOptions -> Aff b (Either Error a)
+
+openautoCompleteModal :: forall eff. FilterOptions -> Aff eff (Either Error ResponseModal)
 openautoCompleteModal opt = attempt $ makeAff (\error success -> _openAutocompleteModal opt success error)
 
-openplacePickerModal :: forall a b. BoundOptions -> Aff b (Either Error a)
+openplacePickerModal :: forall eff. BoundOptions -> Aff eff (Either Error ResponseModal)
 openplacePickerModal opt = attempt $ makeAff (\error success -> _openPlacePickerModal opt success error)
 
-getautocompletePredictions :: forall a b. Query -> Aff b (Either Error a)
+getautocompletePredictions :: forall eff. Query -> Aff eff (Either Error AutocompletePredictionsResponseModal)
 getautocompletePredictions opt = attempt $ makeAff (\error success -> _getAutocompletePredictions opt success error)
 
-lookUpPlaceByID :: forall b. PlaceId -> Aff b (Either Error String)
-lookUpPlaceByID opt = attempt $ makeAff (\error success -> _lookUpPlaceByID opt success error)
+lookUpPlaceByID :: forall eff. PlaceId -> Aff eff (Either Error ResponseModal)
+lookUpPlaceByID (PlaceId opt) = attempt $ makeAff (\error success -> _lookUpPlaceByID (PlaceId opt) success error)
